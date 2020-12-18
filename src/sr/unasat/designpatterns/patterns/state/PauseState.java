3c83de88dea28eba3fe7ae6194d69b90a0e2989a
Maarten Narain <mnarain@qualogy.com>
2020-12-18 18:56:44 -0300

    Template design pattern

--- src/sr/unasat/designpatterns/patterns/state/PauseState.java
+++ src/sr/unasat/designpatterns/patterns/state/PauseState.java
@@ -4,7 +4,7 @@ public class PauseState implements State {
 
    public void doAction(Context context) throws Exception {
       if (!(context.getState() instanceof StartState)) {
-         throw new Exception("Deze handeling kan niet uitgevoerd worden gezien de plater nog niet is gestart.");
+         throw new Exception("Deze handeling kan niet uitgevoerd worden gezien de player nog niet is gestart.");
       }
       System.out.println("Player is in pause state");
       context.setState(this);	
